bfff4e50b8e1906bfcf834da21c3752d07a16e0d
Janik2019 <lucky-lucky-7777@mail.ru>
2019-10-24 17:34:46 +0600

    Ограничил доступ с страницам редактирования, создания и удаления статуса задач для неавторизированнных пользователей

--- source/webapp/views/status_views.py
+++ source/webapp/views/status_views.py
@@ -3,6 +3,7 @@ from django.urls import reverse, reverse_lazy
 from webapp.models import Status
 
 from django.views.generic import ListView,CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class StatusView(ListView):
@@ -11,7 +12,7 @@ class StatusView(ListView):
     model = Status
 
 
-class StatusCreateView(CreateView):
+class StatusCreateView(LoginRequiredMixin,CreateView):
     model = Status
     template_name = 'status/status_create.html'
     form_class = StatusForm
@@ -20,7 +21,7 @@ class StatusCreateView(CreateView):
         return reverse('status')
 
 
-class StatusUpdateView(UpdateView):
+class StatusUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'status/status_update.html'
     form_class = StatusForm
     success_url = reverse_lazy('status')
@@ -28,7 +29,7 @@ class StatusUpdateView(UpdateView):
     context_object_name = 'status'
 
 
-class StatusDeleteView(DeleteView):
+class StatusDeleteView(LoginRequiredMixin, DeleteView):
     form_class = StatusForm
     template_name = 'status/status_delete.html'
     success_url = reverse_lazy('status')
