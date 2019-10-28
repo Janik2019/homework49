9771c04a5bb664b73e28260ea1235c4069cea783
Zhanybek <lucky-lucky-7777@mail.r.ru>
2019-10-28 17:18:25 +0600

    Добавил валидатор, который проверяет, что хотя бы одно из полей first_name и last_name заполнены и сделал емейл поле обязательным

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -4,14 +4,14 @@ from django.core.exceptions import ValidationError
 
 
 class UserCreationForm(forms.Form):
-    username = forms.CharField(max_length=100, label='Логин', required=True)
-    password = forms.CharField(max_length=100, label='Пароль', required=True,
+    username = forms.CharField(max_length=50, label='Логин', required=True)
+    password = forms.CharField(max_length=50, label='Пароль', required=True,
                                widget=forms.PasswordInput)
-    password_confirm = forms.CharField(max_length=100, label='Подтверждение пароля', required=True,
+    password_confirm = forms.CharField(max_length=50, label='Подтверждение пароля', required=True,
                                        widget=forms.PasswordInput)
-    first_name = forms.CharField(max_length=100, required=False, label='Имя')
-    last_name = forms.CharField(max_length=100, required=False, label='Фамилия')
-    email = forms.EmailField(label='Емейл', required=False)
+    first_name = forms.CharField(max_length=50, required=False, label='Имя')
+    last_name = forms.CharField(max_length=50, required=False, label='Фамилия')
+    email = forms.EmailField(label='Емейл', required=True)
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
@@ -26,7 +26,10 @@ class UserCreationForm(forms.Form):
         super().clean()
         password_1 = self.cleaned_data['password']
         password_2 = self.cleaned_data['password_confirm']
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
         if password_1 != password_2:
-            raise ValidationError('Пароли не совпадают',code='passwords_do_not_match')
-        
+            raise ValidationError('Пароли не совпадают', code='passwords_do_not_match')
+        elif not first_name and not last_name:
+            raise ValidationError('Поле имени или фамилии должно быть заполнено', code='last_name_firs_name_error')
         return self.cleaned_data
