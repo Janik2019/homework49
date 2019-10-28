c1f1ad447033a312dd4f73bd7cef066f730ad108
Zhanybek <lucky-lucky-7777@mail.r.ru>
2019-10-28 14:12:10 +0600

    Добавил валидацию на существование пользователя

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserCreationForm(forms.Form):
@@ -11,3 +13,11 @@ class UserCreationForm(forms.Form):
     last_name = forms.CharField(max_length=100, required=False, label='Фамилия')
     email = forms.EmailField(label='Емейл', required=False)
 
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        try:
+            User.objects.get(username=username)
+            raise ValidationError('Такой пользователь уже существует',
+                                  code='user_exists')
+        except User.DoesNotExist:
+            return username
